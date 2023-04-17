e71c9eadc9982100fc955f42f023f67cfd48342e
SXK <61637558+Snipexkillo@users.noreply.github.com>
2023-04-17 03:07:55 -0500

    Update steganography.py

--- steganography.py
+++ steganography.py
@@ -10,62 +10,116 @@ while True: #asks if it will decode or encode
         break
 
 if choice == 'e': #encoding a secret in an image
-
-    print("what info are you trying to hide?:")
-    secret = input()
-    secr = ''
-    for i in secret: #turns secret into binary
-        secr += format(ord(i), "08b")
-    while True: #asks if the image will come from a url or if it is already downloaded
-        print("are you using a url link(u) or a downloaded image with a path on your computer(d)?")
+    while True:  # asks if it will decode or encode
+        print("what data type are you trying to hide? image(i) or text(t)")
         choice = input()
-        if choice == 'u' or choice == 'd':
+        if choice == 'i' or choice == 't':
             break
+    if choice == 'i':
+        while True:  # asks if the image will come from a url or if it is already downloaded
+            print("are you using a url link(u) or a downloaded image with a path on your computer(d) for the image you are trying to hide?")
+            choice = input()
+            if choice == 'u' or choice == 'd':
+                break
 
-    if choice == 'u': #obtains image from url link
-        print('what is your url?')
-        path = input()
-        img = Image.open(urlopen(path))
-    else: #obtains image from computer
-        print("what is the image path?")
-        path = Path(input().strip().replace("\"", ""))
-        img = Image.open(path)
+        if choice == 'u':  # obtains image from url link
+            print('what is your url?')
+            path = input()
+            secrimg = Image.open(urlopen(path))
+        else:  # obtains image from computer
+            print("what is the image path?")
+            path = Path(input().strip().replace("\"", ""))
+            secrimg = Image.open(path)
+
+
+        while True:  # asks if the image will come from a url or if it is already downloaded
+            print("are you using a url link(u) or a downloaded image with a path on your computer(d) for the image that will hide the other image?")
+            choice = input()
+            if choice == 'u' or choice == 'd':
+                break
+        if choice == 'u':  # obtains image from url link
+            print('what is your url?')
+            path = input()
+            img = Image.open(urlopen(path))
+        else:  # obtains image from computer
+            print("what is the image path?")
+            path = Path(input().strip().replace("\"", ""))
+            img = Image.open(path)
+        if (secrimg.height * secrimg.width+ 2*len("00000000000001") + len('{0:08b}'.format(secrimg.width)) + len('{0:08b}'.format(secrimg.height))) >= (img.height * img.width)//4 :
+            secrimg = secrimg.resize((img.size[0]//3, img.size[1]//3))
+
+        #converts all of the RGB(A) into binary
+        secrdata = list(secrimg.getdata())
+        secr = "1"
+        if secrimg.mode == "RGBA":
+            secr += '1'
+        else:
+            secr += '0'
+        secr += '{0:08b}'.format(secrimg.width)
+        secr += "00000000000001"
+        secr += '{0:08b}'.format(secrimg.height)
+        secr += '00000000000001'
+        for x in secrdata:
+            for a in x:
+                secr += '{0:08b}'.format(a)
+
+    if choice == 't':
+        print("what info are you trying to hide?:")
+        secret = input()
+        secr = "00"
+        for i in secret: #turns secret into binary
+            secr += '{0:08b}'.format(ord(i))
+
+        secr += "00000000000001"
+        print(secr)
+        while True: #asks if the image will come from a url or if it is already downloaded
+            print("are you using a url link(u) or a downloaded image with a path on your computer(d)?")
+            choice = input()
+            if choice == 'u' or choice == 'd':
+                break
+
+        if choice == 'u': #obtains image from url link
+            print('what is your url?')
+            path = input()
+            img = Image.open(urlopen(path))
+        else: #obtains image from computer
+            print("what is the image path?")
+            path = Path(input().strip().replace("\"", ""))
+            img = Image.open(path)
 
     siz = img.size #gets image dimensions
-    mod = img.mode #get image type (ex. RGB, RGBA)
+    mod = img.mode == "RGBA" #get image type (ex. RGB, RGBA)
     data = list(img.getdata()) #gets all of the RGB(A) values and puts it in a list
-
     #algo is really simple, you take the least significant bit of each RGB value and make it match a bit of the secret
     #ex. if the RGB is (22, 34, 44) or (00010110, 00100010, 00101100) and the secret is 101
     #we change the RGB to (00010111, 00100010, 00101101) or (23,33,45) which looks the same to the original in our eyes but is different
-
-    for x in range(len(secr)): #iterate through the secret
-
-        if secr[x] == "0" and data[x // 3][x%3]%2==1: #LSB changes only if a change needs to be made
-            li = list(data[x//3]) #get the rgb value as a list
-            li[x%3] -= 1 #edit
-            data[x//3] = tuple(li) #turn the edited rgb list into a tuple and put back in data list
-
-        elif secr[x] == "1" and data[x // 3][x%3]%2==0: #LSB changes only if a change needs to be made
-            li = list(data[x // 3]) #get the rgb value as a list
-            li[x % 3] += 1 #edit
-            data[x // 3] = tuple(li) #turn the edited rgb list into a tuple and put back in data list
-
+    li = []
+    for x in range(len(secr)//2): #iterate through the secret
+        if x % 3 == 0:
+            li = list(data[x // 3])
+        li[x % 3] = format(li[x % 3], "08b")[0:6] + (secr[x*2:x*2+2])
+        if x % 3 == 2:
+            nli = [int(li[i], 2) for i in range(3)]
+            if mod :
+                nli.append(li[3])
+            data[x // 3] = tuple(nli) #turn the edited rgb list into a tuple and put back in data list
+    if (len(secr)//2)%3 != 2:
+        for x in range(3):
+            if isinstance(li[x], int):
+                li[x] = '{0:08b}'.format(li[x])
+        nli = [int(li[i], 2) for i in range(3)]
+        if mod:
+            nli.append(li[3])
+        data[x // 3] = tuple(nli)  # turn the edited rgb list into a tuple and put back in data list
     le = len(secr)
-    # a simple way to end transmission, if you get 12 0's and a 1 then decoder knows transmission is over
-    for x in range(12): #adds the 12 0's
-        li = list(data[(le + x) // 3])
-        if li[(le + x) %3] %2 == 1:
-            li[(le + x) % 3] -= 1
-            data[(le + x) // 3] = tuple(li)
-    li = list(data[(le + 12) // 3]) #adds the one
-    li[(le + 12) % 3] = 1
-    data[(le + 12) // 3] = tuple(li)
 
     #create the new image
-    newimg = Image.new(mod, siz)
+    newimg = Image.new(img.mode, siz)
     newimg.putdata(data)
     newimg.show()
+    for a in range(10):
+        print(data[a])
+        print(list(newimg.getdata())[a])
 
     while True:  #asks if image will be saved
         print("Would you like this image saved? (y or n)")
@@ -92,7 +146,7 @@ if choice == 'e': #encoding a secret in an image
                 downloadfolder = f"{os.getenv('USERPROFILE')}\\Downloads"
             else:  # PORT: For *Nix systems
                 downloadfolder = f"{os.getenv('HOME')}/Downloads"
-            print(downloadfolder + name)
+            print(downloadfolder + "\\" + name)
             newimg.save(downloadfolder + "\\" + name)
     #newimg.save("C:\\Users\\aksha\Downloads\\newimg.png")
 
@@ -117,21 +171,54 @@ if choice == 'd':
     siz = img.size  # gets image dimensions
     mod = img.mode  # get image type (ex. RGB, RGBA)
     data = list(img.getdata())  # gets all of the RGB(A) values and puts it in a list
+    for a in range(10):
+        print(data[a])
+    secr = '{0:02b}'.format(data[0][0] % 4)
+    print(secr)
+    if secr[0] == '0':
+        secr = ''
+        condition = 0
+        for x in range(1, len(data)*3): #gets the last 2 bits of data from each RGB value
+            secr += '{0:02b}'.format(data[x // 3][x % 3] % 4)
 
-
-    secr = ""
-    condition = 0
-    for x in range(len(data)*3): #gets the last bit of data from each RGB value
-        secr += str(data[x // 3][x % 3] % 2)
-        if data[x // 3][x % 3] % 2 == 0: #checks for the condition that ends transmission (0000000000001)
-            condition += 1
-        else:
-            if condition >= 12:
-                break
+            if data[x // 3][x % 3] % 4 == 0: #checks for the condition that ends transmission (0000000000001)
+                condition += 1
             else:
-                condition = 0
+                if condition >= 6:
+                    break
+                else:
+                    condition = 0
+    else:
+        rgb = secr[1] == 0
+        width = ""
+        height = ""
+        counter = 1
+        for x in range(1, len(data)*3, 1): #gets the last bit of data from each RGB value
+            counter+=1
+            width += '{0:02b}'.format(data[x // 3][x % 3] % 4)
+            if data[x // 3][x % 3] % 4 == 0: #checks for the condition that ends transmission (0000000000001)
+                condition += 1
+            else:
+                if condition >= 6:
+                    break
+                else:
+                    condition = 0
+        for x in range(counter, len(data)*3, 1): #gets the last bit of data from each RGB value
+            counter+=1
+            height += '{0:02b}'.format(data[x // 3][x % 3] % 4)
+            if data[x // 3][x % 3] % 4 == 0: #checks for the condition that ends transmission (0000000000001)
+                condition += 1
+            else:
+                if condition >= 6:
+                    break
+                else:
+                    condition = 0
+        width = int(width[0:len(width)-14], 2)
+        height = int(width[0:len(height) - 14], 2)
 
-    secr = secr[0:len(secr)-13] #removes unnecessary bits from the condition
+
+    print(secr)
+    secr = secr[0:len(secr)-14] #removes unnecessary bits from the condition
     sec = ""
     for x in range((len(secr)+7)//8): #turns binary into ascii/readable text
         sec += chr(int(secr[x*8:x*8+8], 2))
