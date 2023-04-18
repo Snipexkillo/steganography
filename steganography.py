42f521516bf9b50e71639e1fd7967faaf9d98285
Snipexkillo <akshah1201@gmail.com>
2023-04-17 22:03:46 -0500

    Update steganography.py

--- steganography.py
+++ steganography.py
@@ -170,8 +170,9 @@ if choice == 'd':
     mod = img.mode  # get image type (ex. RGB, RGBA)
     data = list(img.getdata())  # gets all of the RGB(A) values and puts it in a list
     secr = '{0:02b}'.format(data[0][0] % 4)
+    check = secr[0] == "0"
     print(secr)
-    if secr[0] == '0':
+    if check:
         secr = ''
         condition = 0
         for x in range(1, len(data)*3): #gets the last 2 bits of data from each RGB value
@@ -189,6 +190,7 @@ if choice == 'd':
         width = ""
         height = ""
         counter = 1
+        condition = 0
         for x in range(1, len(data)*3, 1): #gets the last bit of data from each RGB value
             counter+=1
             width += '{0:02b}'.format(data[x // 3][x % 3] % 4)
@@ -199,6 +201,7 @@ if choice == 'd':
                     break
                 else:
                     condition = 0
+        condition = 0
         for x in range(counter, len(data)*3, 1): #gets the last bit of data from each RGB value
             counter+=1
             height += '{0:02b}'.format(data[x // 3][x % 3] % 4)
@@ -210,12 +213,67 @@ if choice == 'd':
                 else:
                     condition = 0
         width = int(width[0:len(width)-14], 2)
-        height = int(width[0:len(height) - 14], 2)
+        height = int(height[0:len(height) - 14], 2)
+        secr = ''
+        condition = 0
+        for x in range(counter, counter + width*height):  # gets the last 2 bits of data from each RGB value
+            secr += '{0:02b}'.format(data[x // 3][x % 3] % 4)
 
+            if data[x // 3][x % 3] % 4 == 0:  # checks for the condition that ends transmission (0000000000001)
+                condition += 1
+            else:
+                if condition >= 6:
+                    break
+                else:
+                    condition = 0
 
-    print(secr)
-    secr = secr[0:len(secr)-15] #removes unnecessary bits from the condition
-    sec = ""
-    for x in range((len(secr)+7)//8): #turns binary into ascii/readable text
-        sec += chr(int(secr[x*8:x*8+8], 2))
-    print(sec) #prints decoded secret
+    print(width)
+    print(height)
+    print(len(secr))
+    if check:
+        secr = secr[0:len(secr)-14] #removes unnecessary bits from the condition
+        sec = ""
+        for x in range(len(secr)//8): #turns binary into ascii/readable text
+            sec += chr(int(secr[x*8:x*8+8], 2))
+        print(sec) #prints decoded secret
+    else:
+        dat = []
+        for x in range(0, len(secr)//8, 3):
+            dat.append(tuple([secr[x*24:x*24 + 8], secr[x*24 + 8:x*24 + 16], secr[x*24 + 16:x*24 + 24]]))
+
+            # create the new image
+        newimg = Image.new(img.mode, siz)
+        newimg.putdata(data)
+        newimg.show()
+        zdat = list(img.getdata())
+        ndat = list(newimg.getdata())
+
+        while True:  # asks if image will be saved
+            print("Would you like this image saved? (y or n)")
+            choice = input()
+            if choice == 'y' or choice == 'n':
+                break
+
+        if choice == 'y':  # image will be saved
+            while True:  # asks where it will be saved?
+                print(
+                    "Would you like it saved towards a specific path (p) or just downloaded with a specific name (n)?")
+                choice = input()
+                if choice == 'p' or choice == 'n':
+                    break
+
+            if choice == 'p':  # gets path and saves image there
+                print("what is your path")
+                path = Path(input().strip().replace("\"", ""))
+                newimg.save(path)
+
+            elif choice == 'n':  # gets filename and saves image with it
+                print('what is the filename (include .png/jpg)')
+                name = input().strip().replace("\"", "")
+                if os.name == "nt":
+                    downloadfolder = f"{os.getenv('USERPROFILE')}\\Downloads"
+                else:  # PORT: For *Nix systems
+                    downloadfolder = f"{os.getenv('HOME')}/Downloads"
+                print(downloadfolder + "\\" + name)
+                newimg.save(downloadfolder + "\\" + name)
+        # newimg.save("C:\\Users\\aksha\Downloads\\newimg.png")
